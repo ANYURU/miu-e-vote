dc84a206f7b077bccee1e7e81d80dd79a7404352
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-01-11 15:57:46 +0300

    chore: updated the candidates view

--- frontend/static/js/views/CandidatesView.js
+++ frontend/static/js/views/CandidatesView.js
@@ -1,20 +1,15 @@
 import AbstractView from "./AbstractView.js";
+import { supabaseClient } from "../index.js";
 
 export default class extends AbstractView {
-
-  constructor(supabase) {
-    super();
-    this.setTitle("candidates");
-    
+  constructor(params) {
+    super(params);
+    this.setTitle("Candidates");
+    this.openNav = false;
   }
 
-  
-
-  getHtml () {
-    // Construct the protected layout render the side bar and render
-    // 
-    return "<h1>Candidates</h1>"
+  async renderContent() {
+    const layout = await this.renderProtectedLayout();
+    return layout;
   }
-
-
 }
